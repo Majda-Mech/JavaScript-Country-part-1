fb60f37d13dee53e65e4c72c0613d67d8036b549
Mechda <mechda.zariouh@gmail.com>
2022-11-14 15:32:36 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+async function fetchCountryInformation(){
+
+    const BASE_URI = 'https://restcountries.com'
+    const ENDPOINT = 'v3.1/all'
+
+    try {
+        const response = await axios.get('BASE_URI + ENDPOINT');
+        const { data: countries } = response;
+
+        const countryList = document.getElementById("countries");
+
+        countries.map((country) => {
+            country.innerHTML = `
+                <li>
+                    <h3 class="countries-${countries.region}">$</h3>
+                    <p>Has a population of $ { countries [0].polution } people</p>
+                    <img src="" alt"${countries[0].population}>
+                </li>
+               `
+    })
+
+    } catch ( error ){
+        console.error( error )
+    }}
+fetchCountryInformation()
