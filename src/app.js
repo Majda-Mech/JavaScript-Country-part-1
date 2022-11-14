ec4f60ca23481311ee3b0d11382624c8762049b0
Majda Zariouh Zariouh <112877888+Majda-Mech@users.noreply.github.com>
2022-11-14 23:58:19 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
 import axios from "axios";
 
 async function fetchCountryInformation(){
-
-    const BASE_URI = 'https://restcountries.com'
-    const ENDPOINT = 'v3.1/all'
-
+    const BASE_URI = 'https://restcountries.com';
+    const ENDPOINT = 'v3.1/all';
     try {
         const response = await axios.get('BASE_URI + ENDPOINT');
-        const { data: countries } = response;
+        console.log(countries.data[0]);
 
+        const { data: countries } = response;
         const countryList = document.getElementById("countries");
 
         countries.map((country) => {
-            country.innerHTML = `
+            countryList.innerHTML += `
                 <li>
-                    <h3 class="countries-${countries.region}">$</h3>
-                    <p>Has a population of $ { countries [0].polution } people</p>
-                    <img src="" alt"${countries[0].population}>
+                    <h3 class="countries-">${country[0].region}">${countries[0].name}</h3>
+                    <p>Has a population of ${country.data[0].polution} people</p>
+                    <img src="${country[0].flags.png}" alt="${country[0].name}"/>
                 </li>
                `
-    })
-
-    } catch ( error ){
+        })
+    }catch ( error ){
         console.error( error )
-    }}
+    }
+}
 fetchCountryInformation()
